a402d5375016261bda6ebca3d6a3bc261d5db3ed
ianis-dev <ianischennaf@outlook.com>
2023-02-14 10:16:44 +0100

    merge

--- src/components/MaisonCardRow.vue
+++ src/components/MaisonCardRow.vue
@@ -1,11 +1,4 @@
 <template>
-<<<<<<< HEAD
-  <div class="maison--row">
-    <div class="wrapper--card">
-      <MaisonCard v-for="(card, index) in 4" :key="index" />
-    </div>
-  </div>
-=======
     <div class="Container">
         <div class="maison--row">
             <div class="wrapper--card">
@@ -13,14 +6,12 @@
             </div>
         </div>
     </div>
->>>>>>> f5bc85fbaffe116e5b0d644b66601a559ae3f1b1
 </template>
 
 <script>
 import MaisonCard from "./MaisonCard.vue";
 
 export default {
-<<<<<<< HEAD
   name: "MaisonRow",
   components: {
     MaisonCard,
@@ -40,27 +31,3 @@ export default {
   }
 }
 </style>
-=======
-    name: "MaisonRow",
-    components: {
-        MaisonCard
-    }
-}
-</script>
-
-<style lang="scss">
-.container{
-    width: 20vh;
-    height:10vh;
-    background-color:red;
-}
-.maison--row{
-    .wrapper--card{
-        display:flex;
-        justify-content: space-between;
-        align-items: center;
-        margin: 5vh;
-    }
-}
-</style>
->>>>>>> f5bc85fbaffe116e5b0d644b66601a559ae3f1b1
