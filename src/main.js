497644ad4b0676c631810164c138f7bccbf3e3a8
ianis-dev <ianischennaf@outlook.com>
2023-02-14 09:36:30 +0100

    pull

--- src/main.js
+++ src/main.js
@@ -5,17 +5,7 @@ import './assets/main.css'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 
-/* import the fontawesome core */
-import { library } from '@fortawesome/fontawesome-svg-core'
-
-/* import font awesome icon component */
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-/* import specific icons */
-import { faUser, faUserSecret } from '@fortawesome/free-solid-svg-icons'
-
-/* add icons to the library */
 library.add(faUser, faUserSecret)
 
-createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
+createApp(App).mount('#app')
 
