403d759129301d1a4373389f6e4dce6ea165925a
arthur277 <arthurdeumenitsako@gmail.com>
2023-02-14 09:50:57 +0100

    new

--- src/components/MaisonCardRow.vue
+++ src/components/MaisonCardRow.vue
@@ -1,32 +1,31 @@
 <template>
   <div class="maison--row">
     <div class="wrapper--card">
-        <MaisonCard v-for="(card, index) in 4" :key="index"/>
-  </div>
+      <MaisonCard v-for="(card, index) in 4" :key="index" />
+    </div>
   </div>
 </template>
 
 <script>
-import MaisonCard from './MaisonCard.vue';
+import MaisonCard from "./MaisonCard.vue";
 
 export default {
-    name: "MaisonRow",
-    components : {
-        MaisonCard
-    }
-}
+  name: "MaisonRow",
+  components: {
+    MaisonCard,
+  },
+};
 </script>
 
 <style lang="scss">
-    .maison--row{
-        width: 600vh;
-        display:flex;
-        justify-content: space-between;
-        .wrapper--card {
-            display:flex;
-            align-items: center;
-            justify-content: space-between;
-        
-        }
-    }
+.maison--row {
+  width: 600vh;
+  display: flex;
+  justify-content: space-between;
+  .wrapper--card {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+}
 </style>
